a8d297e94d93cb077040d0fb5cec6310bced90c4
Paul Schattenberg <schattenberg@msn.com>
2011-12-26 17:51:28 -0600

    Forgot to uncomment algorithm for atmospheric refraction.

--- predict.c
+++ predict.c
@@ -4394,7 +4394,7 @@ double daynum;
 	az=atan2(sin(h),cos(h)*sin(n)-tan(dec)*cos(n))+pi;
 	el=asin(sin(n)*sin(dec)+cos(n)*cos(dec)*cos(h));
 	
-//	el=el+((1./tan(el/deg2rad+7.31/(el/deg2rad+4.4)))/60.)*deg2rad;
+	el=el+((1./tan(el/deg2rad+7.31/(el/deg2rad+4.4)))/60.)*deg2rad;
 	
 	moon_az=az/deg2rad;
 	moon_el=el/deg2rad;
